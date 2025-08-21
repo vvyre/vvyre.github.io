11a34e7da490d51ef3ce689c799f6296b9bf04cf
vvyre <seung.yoon.yu@gmail.com>
2025-08-21 18:16:31 +0900

    페이지 배포 구조 변경

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  base: `/vvyre.github.io/`,
   plugins: [react({ jsxImportSource: '@emotion/react' }), tsconfigPaths()],
 })
